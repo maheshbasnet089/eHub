c51de0737dbdc4a4830a152e3ea10004a0370dd6
manishbasnet089 <basnetmanish089@gmail.com>
2021-11-14 18:26:54 +0545

    hashed otp sent

--- backend/controllers/otp-controller.js
+++ backend/controllers/otp-controller.js
@@ -5,13 +5,24 @@ class OtpController {
   async sendOtp(req, res) {
     const { phone } = req.body;
     if (!phone) {
-      return res.status(400).json({ message: "Phone Number is required" });
+      return res.status(400).json({ message: "Phone field is required" });
     }
     const otp = await OtpService.generateOtp();
     const expiresIn = Date.now() + 1000 * 60 * 2;
     const data = `${phone}.${otp}.${expiresIn}`;
     const hash = HashServices.hashOtp(data);
-    res.json({ hash });
+    try {
+      res.json({
+        hash: `${hash}.${expiresIn}`,
+        phone,
+        otp,
+      });
+    } catch (e) {
+      console.log(e);
+      res.status(500).json({
+        message: "Message sending failed ",
+      });
+    }
   }
 }
 
