f328ce3aebfc4aa06ac755850d02a47a82640f38
manishbasnet089 <basnetmanish089@gmail.com>
2021-11-16 16:39:54 +0545

    stepName complete

--- frontend/src/pages/Steps/StepName/StepName.jsx
+++ frontend/src/pages/Steps/StepName/StepName.jsx
@@ -1,10 +1,37 @@
-import React from 'react';
+import React, { useState } from 'react';
+import Card from '../../../components/shared/Card/Card';
+import TextInput from '../../../components/shared/TextInput/TextInput';
+import Button from '../../../components/shared/Button/Button';
+import styles from './StepName.module.css'
+import { useDispatch,useSelector } from 'react-redux';
+import { setName } from '../../../store/activateSlice';
 
 const StepName = ({ onNext }) => {
+    const dispatch = useDispatch()
+    const [fullname,setFullname] = useState(useSelector(state => state.activateSlice.name))
+    function submit(){
+        if(!fullname){
+            return
+        }
+        dispatch(setName(fullname))
+        onNext()
+    }
+
     return (
         <>
-            <div>Name component</div>
-            <button onClick={onNext}>Next</button>
+    <Card title="What is your Name?" icon="goggle-emoji">
+        <TextInput value={fullname} onChange={(e)=>{setFullname(e.target.value)}} />
+        <div>
+        <p className={styles.paragraph}>
+                    Please enter your real name to enter eHub :-)
+         </p>
+        <div className={styles.actionButtonWrap}>
+            <Button text="Next" onClick={submit} />
+
+            </div>
+        </div>
+
+     </Card>
         </>
     );
 };
--- frontend/src/store/index.js
+++ frontend/src/store/index.js
@@ -1,8 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
 import auth from "./authSlice";
+import activateSlice from "./activateSlice";
 
 export const store = configureStore({
   reducer: {
     auth,
+    activateSlice,
   },
 });
